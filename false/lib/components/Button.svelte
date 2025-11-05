99b4f1a6ec30bae213ea8b108d9edf8ec5cd79e8
Adithiya Venkatakrishnan <adithiya.venkatakrishnan@atomtables.dev>
2025-11-04 21:58:09 -0500

    and new stuff

---
+++ false/lib/components/Button.svelte
@@ -0,0 +1,24 @@
+<script>
+
+
+    let innerWidth;
+    let innerHeight;
+
+    export let url = "";
+    export let className = "";
+
+    export let icon;
+
+</script>
+
+<a href={url}
+   class="inline-block text-base text-center transition-all duration-250 bg-green-950 bg-opacity-50 backdrop-blur-xl rounded-full shadow-2xl  text-nowrap overflow-ellipsis cursor-pointer hover:bg-scheme-green-200 active:bg-scheme-green-300 h-inherit {className}">
+        <span class="flex content-center align-middle h-12">
+            <span class="place-self-center h-full rounded-full grid place-items-center aspect-square border-white">
+                <img src={icon} alt="icon" class="block h-8"/>
+            </span>
+            <span class="border-gray-800 py-4 pr-4 text-2xl place-self-center font-bold">
+                <slot/>
+            </span>
+        </span>
+</a>
---
+++ false/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="w-full p-10 backdrop-blur-xl flex justify-center my-20">
+    <img src="/theatomblog.png" alt="the atom blog's logo" class="max-w-4xl">
+</div>
+
+<div>
+
+</div>
